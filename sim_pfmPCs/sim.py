93be5844ba4f3d428fea7236bfa9d9ea5475b33a
Yakir Reshef <yr073@cn007.research.partners.org>
2020-08-07 12:02:48 -0400

    pfm pcs now with interpretability

--- sim_pfmPCs/sim.py
+++ sim_pfmPCs/sim.py
@@ -1,6 +1,7 @@
 # Import Package Dependencies
 import pickle, argparse
 import numpy as np
+import pandas as pd
 import scanpy as sc
 import paths, simulation
 from methods import methods
@@ -14,7 +15,6 @@ parser.add_argument('--method')
 parser.add_argument('--index', type=int)
 parser.add_argument('--noise-level', type=float) #in units of std dev of noiseless phenotype
 args = parser.parse_args()
-
 print('\n\n****')
 print(args)
 print('****\n\n')
@@ -22,36 +22,33 @@ print('****\n\n')
 # Read Data
 data = sc.read(paths.tbru_h5ad + args.dset + '.h5ad', backed = "r")
 sampleXmeta = data.uns['sampleXmeta']
-    
-# simulate phenotype
+
+# Simulate phenotype
 np.random.seed(args.index)
 n_phenotypes = 20 #Number of PFM PCs to test
-sim_pcs = np.arange(n_phenotypes)                                                                                   
-phenotype_names = ["causal_PFM_PC" + s for s in sim_pcs.astype("str")]
+sim_pcs = np.arange(n_phenotypes)
+pheno_names = ['PFM' + s for s in sim_pcs.astype('str')]
 
-for i_phenotype in np.arange(n_phenotypes):
-    n_pc = sim_pcs[i_phenotype]
-    data.obs[phenotype_names[i_phenotype]] = data.uns['sampleXnh_featureXpc'][:,i_phenotype]
-    sampleXmeta[phenotype_names[i_phenotype]] = data.uns['sampleXnh_sampleXpc'][:,i_phenotype]
-
-Ys = sampleXmeta[phenotype_names].values.T
+#TODO: revisit interpretability for PFM pcs. Does this phenotype fit in the cell score model?
+true_cell_scores = pd.DataFrame(
+    data.uns['sampleXnh_featureXpc'][:,:n_phenotypes], columns=pheno_names)
+Ys = pd.DataFrame(data.uns['sampleXnh_sampleXpc'][:,:n_phenotypes], columns=pheno_names,
+    index=sampleXmeta.index).T
 print(Ys.shape)
 
-# Add noise                                                                                                                                             
-Yvar = np.std(Ys, axis=1)
-noiselevels = args.noise_level * Yvar
-noise = np.random.randn(*Ys.shape) * noiselevels[:,None]
-Ys = Ys + noise
+# Add noise
+Ys = simulation.add_noise(Ys, args.noise_level)
 
 # Execute Analysis
 res = simulation.simulate(
         args.method,
         data,
-        Ys,
+        Ys.values,
         sampleXmeta.batch.values,
         sampleXmeta.C.values,
         None, # no sample-level covariates
-        None) # no cellular covariates
+        None,
+        true_cell_scores.T) # no cellular covariates
 res['phenotype'] = phenotype_names
 
 # Assess correlation of true and estimated neighborhood values                                                                                                                        
