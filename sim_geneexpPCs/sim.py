1d5822505b00bbb4009d5dc2dc3f10b8652263a2
Yakir Reshef <yr073@cn002.research.partners.org>
2021-01-03 00:13:12 -0500

    new interpretability, new output format

--- sim_geneexpPCs/sim.py
+++ sim_geneexpPCs/sim.py
@@ -1,8 +1,7 @@
-# Import Package Dependencies
 import pickle, argparse
 import numpy as np
 import pandas as pd
-import scanpy as sc
+import cna
 import paths, simulation
 from scipy import stats
 
@@ -18,11 +17,9 @@ print('\n\n****')
 print(args)
 print('****\n\n')
 
-### Simulate Phenotype
-
 # load dataset
-data = sc.read(paths.tbru_h5ad + args.dset +'.h5ad', backed = "r")
-sampleXmeta = data.uns['sampleXmeta']
+data = cna.read(paths.tbru_h5ad + args.dset +'.h5ad')
+sampleXmeta = data.samplem
 if args.dset[0:4]=="harm":
     data.obsm['X_pca'] = data.X
 
@@ -30,7 +27,7 @@ if args.dset[0:4]=="harm":
 np.random.seed(args.index)
 n_phenotypes = 20
 sim_pcs = np.arange(n_phenotypes)
-pheno_names = ["causal_PC" + s for s in sim_pcs.astype("str")]
+pheno_names = ["PC" + s for s in sim_pcs.astype("str")]
 true_cell_scores = pd.DataFrame(data.obsm['X_pca'][:,:n_phenotypes], columns=pheno_names,
                                 index=data.obs.index)
 Ys = simulation.avg_within_sample(data, true_cell_scores)
@@ -40,7 +37,7 @@ print(Ys.shape)
 Ys = simulation.add_noise(Ys, args.noise_level)
 
 # Execute Analysis
-res = simulation.simulate(
+for i, res in enumerate(simulation.simulate(
         args.method,
         data,
         Ys.values,
@@ -49,13 +46,12 @@ res = simulation.simulate(
         None, # no sample-level covariates
         None, # no cellular covariates
         true_cell_scores.T,
-        False, # store true and estimated cell scores
-        False) # filter out phenotypes with correlation to batch
-res['phenotype'] = pheno_names
+        report_cell_scores=False,
+        QC_phenotypes=False)):
 
-# Write Results to Output File(s)
-outfile = paths.simresults(args.dset, args.simname) + str(args.index) + '.p'
-print('writing', outfile)
-pickle.dump(res, open(outfile, 'wb'))
-print('done')
+    # add phenotype id
+    vars(res)['id'] = str(args.index)+'.'+res.pheno
 
+    # write results
+    outfile = '{}{}.{}.p'.format(paths.simresults(args.dset, args.simname), args.index, i)
+    pickle.dump(res, open(outfile, 'wb'))
