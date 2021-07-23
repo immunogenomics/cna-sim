201e3b1dc6183dd3f6588353ac68dfc4583699c8
Yakir Reshef <yr073@cn002.research.partners.org>
2021-07-23 10:11:38 -0400

    realphenos

--- sim_realphenos/sim.py
+++ sim_realphenos/sim.py
@@ -1,61 +1,64 @@
 # Import Package Dependencies
 import pickle, argparse, os
 import numpy as np
-import scanpy as sc
+import cna
+import pandas as pd
 import paths, simulation
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--dset')
 parser.add_argument('--simname')
-parser.add_argument('--method', nargs='+')
+parser.add_argument('--method')
 parser.add_argument('--index', type=int)
 parser.add_argument('--noise-level', type=float) #in units of std dev of noiseless phenotype
 args = parser.parse_args()
-
 print('\n\n****')
 print(args)
 print('****\n\n')
 
 # Read Data
-data = sc.read(paths.simdata + args.dset + '.h5ad')
-sampleXmeta = data.uns['sampleXmeta']
+data = cna.read(paths.simdata + args.dset + '.h5ad')
+sampleXmeta = data.samplem
+sampleXmeta['age2'] = sampleXmeta.age**2
 
 # simulate phenotype
 np.random.seed(args.index)
-phenotypes = ['Weight','NATad4KR','TB_STATUS_CASE','Sex_M','season_Winter','age']
+phenos = ['TB_STATUS_CASE','EURad4KR','Sex_M','season_Winter','age']
+covs = {
+    'TB_STATUS_CASE':['age','age2','Sex_M','season_Winter','EURad4KR'],
+    'EURad4KR':['Height','Weight','edu_cat_belowHighschool','season_Winter','TB_STATUS_CASE'],
+    'Sex_M':['Height','num_scar','TB_STATUS_CASE'],
+    'season_Winter':['age','Height','EURad4KR','BCG_scar','season_Spring'],
+    'age':['Weight','num_scar','season_Winter','TB_STATUS_CASE'],
+}
 
-all_Ys = sampleXmeta[phenotypes].values.T
-print(phenotypes)
-print(all_Ys.shape)
+Ys = sampleXmeta[phenos].T
+print(phenos)
+print(Ys.shape)
 
 # Add noise
-all_Yvar = np.std(all_Ys, axis=1)
-noiselevels = args.noise_level * all_Yvar
-noise = np.random.randn(*all_Ys.shape) * noiselevels[:,None]
-all_Ys = all_Ys + noise
-
-scovs = np.array([
-    sampleXmeta[[p_ for p_ in phenotypes if p_ != p]].values
-    for p in phenotypes])
+Ys = simulation.add_noise(Ys, args.noise_level)
 
 # Execute Analysis
-for m in args.method:
-    res = simulation.simulate(
-        m,
-        data,
-        all_Ys,
-        sampleXmeta.batch.values,
-        sampleXmeta.C.values,
-        scovs,
-        None) # DO NOT CURRENTLY ACCOUNT FOR CELLULAR COVARS
-    res['phenotypes'] = np.array(phenotypes)
+for i, (pheno, Y) in enumerate(Ys.iterrows()):
+    res = next(
+        simulation.simulate(
+            args.method,
+            data,
+            Y.values.reshape((1,-1)),
+            sampleXmeta.batch.values,
+            sampleXmeta.C.values,
+            sampleXmeta[covs[pheno]].values,
+            None, # DO NOT CURRENTLY ACCOUNT FOR CELLULAR COVARS
+            pd.DataFrame(np.zeros((1,len(data.obs))), index=[pheno]),
+            report_cell_scores=False,
+            QC_phenotypes=False)
+        )
+
+    # add pheno id
+    vars(res)['id'] = str(args.index)+'.'+res.pheno
 
     # Write Results to Output File(s)
-    outdir = paths.simresults(args.dset, args.simname) + m + '/'
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    outfile = outdir + str(args.index) + '.p'
-    print('writing', outfile)
+    outfile = '{}{}.{}.p'.format(paths.simresults(args.dset, args.simname), args.index, i)
     pickle.dump(res, open(outfile, 'wb'))
-    print('done')
